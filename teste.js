d6aa76e3b8b404d4ea404818bf27e21c067d4f49
Jeovana Santos da Costa <jeovanasantosdacosta25@gmail.com>
2024-08-29 15:22:15 -0400

    acesso a arquivos json

---
+++ teste.js
@@ -0,0 +1,6 @@
+const fs = require("fs")
+const dadosAtuais = JSON.parse(fs.readFileSync("livros.json"))
+const novoDado = { id: '3', nome: 'livro3'}
+fs.writeFileSync("livros.json", JSON.stringify([...dadosAtuais, novoDado ]))
+
+console.log()
