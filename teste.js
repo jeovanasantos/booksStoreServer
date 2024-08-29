51fcb4eba3f9cebea98b46ba996f82f46388979e
Jeovana Santos da Costa <jeovanasantosdacosta25@gmail.com>
2024-08-29 17:02:57 -0400

    criando serviços

--- teste.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-const dadosAtuais = JSON.parse(fs.readFileSync("livros.json"))
-const novoDado = { id: '3', nome: 'livro3'}
-fs.writeFileSync("livros.json", JSON.stringify([...dadosAtuais, novoDado ]))
-
-console.log()
